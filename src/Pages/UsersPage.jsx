39384cc4e783225abffb32dbb4909ac3fd9cb6e5
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-02-11 03:37:05 +0600

    nav added, add user modal added

---
+++ src/Pages/UsersPage.jsx
@@ -0,0 +1,244 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { CiSearch } from "react-icons/ci";
+
+
+const UsersPage = () => {
+  const [users, setUsers] = useState([]);
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((data) => data.json())
+      .then((finalData) => setUsers(finalData));
+  }, []);
+
+  console.log(users.users);
+
+
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {console.log(data) , reset()}
+
+
+  return (
+    <>
+      <div className="w-full h-full bg-slate-300">
+        <div className="h-20 w-full flex items-center justify-center space-x-6 bg-sky-100">
+          <div id="search_box">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Search By Dua Name"
+                className="px-4 py-3 rounded-md text-sm font-medium outline-none border-none focus:outline-sky-300 focus:outline-2 bg-white"
+              />
+              <div className="absolute my-1 mr-1 bottom-0 h-auto top-0 right-0 bg-white w-[17%] flex items-center justify-center rounded-md cursor-pointer">
+                <CiSearch fill="gray" className="w-5 h-5" />
+              </div>
+            </div>
+          </div>
+          <div id="selection">
+            <select
+              name="HeadlineAct"
+              id="HeadlineAct"
+              className="px-3 py-[11px] w-full rounded-md border-gray-300 text-sm font-medium text-gray-700 outline-none border-none"
+            >
+              <option value="">Please Select</option>
+              <option value="JM">John Mayer</option>
+              <option value="SRV">Stevie Ray Vaughn</option>
+              <option value="JH">Jimi Hendrix</option>
+              <option value="BBK">B.B King</option>
+              <option value="AK">Albert King</option>
+              <option value="BG">Buddy Guy</option>
+              <option value="EC">Eric Clapton</option>
+            </select>
+          </div>
+
+          <div id="add_user">
+            <button
+              className="btn bg-white hover:bg-white hover:border-2 hover:border-sky-300"
+              onClick={() => document.getElementById("my_modal_4").showModal()}
+            >
+              <p className="text-sm font-medium">Add User</p>
+            </button>
+            <dialog id="my_modal_4" className="modal">
+              <div className="modal-box w-11/12 max-w-full">
+
+                {/* //////////////////////// */}
+
+                <div className="flex items-center justify-between space-x-4">
+                    <div className="md:basis-2/5 px-6 h-full">
+                          <div className="mt-48 md:mt-0">
+                            <div className="border-b-black border-b-[3px] pb-6 flex items-center justify-center my-10">
+                                <div className="text-3xl font-semibold">
+                                  <p>
+                    Add User Form
+                                  </p>
+                                </div>
+                              </div>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                              {/* <p className="py-5 font-semibold text-gray-800 text-base">
+                                Class Name
+                              </p>
+                              <div className="relative z-0 w-full mb-6 group">
+                                <select {...register("c_name")} className="select select-info w-full max-w-xs text-gray-800">
+                                  <option className="text-gray-800" value="Piano Prowess: Keys to Excellence">Piano</option>
+                                  <option className="text-gray-800" value="Guitar Mastery: Fretboard Fundamentals ">Guiter</option>
+                                  <option className="text-gray-800" value="Violin Virtuosity: Bow to Strings">Violin</option>
+                                  <option className="text-gray-800" value="Drum Dynamics: Rhythm Revolution">Drums</option>
+                                  <option className="text-gray-800" value="Bass Groove: Low-End Explorations">Bass</option>
+                                  <option className="text-gray-800" value="Saxophone Serenade: Wind Harmony">Saxophone</option>
+                                </select>
+                              </div> */}
+                              <div className="relative z-0 w-full mb-6 group">
+                                <input
+                                  {...register("image")}
+                                  type="url"
+                                  id="floating_repeat_password"
+                                  className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                  required
+                                />
+                                <label
+                                  htmlFor="floating_repeat_password"
+                                  className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                  Avatar
+                                </label>
+                              </div>
+                              <div className="relative z-0 w-full mb-6 group">
+                                <input
+                                  {...register("firstName")}
+                                  type="text"
+                                  id="floating_repeat_password"
+                                  className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                //   value={user?.displayName || ""}
+                                />
+                                <label
+                                  htmlFor="floating_repeat_password"
+                                  className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                  First Name
+                                </label>
+                              </div>
+                              <div className="relative z-0 w-full mb-6 group">
+                                <input
+                                  {...register("lastName")}
+                                  type="text"
+                                  id="floating_repeat_password"
+                                  className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                //   value={user?.displayName || ""}
+                                />
+                                <label
+                                  htmlFor="floating_repeat_password"
+                                  className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                  Last Name
+                                </label>
+                              </div>
+                              <div className="relative z-0 w-full mb-6 group">
+                                <input
+                                  {...register("email")}
+                                  type="email"
+                                  id="floating_repeat_password"
+                                  className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                //   value={user?.email || ""}
+                                />
+                                <label
+                                  htmlFor="floating_repeat_password"
+                                  className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                >
+                                  Email
+                                </label>
+                              </div>
+                              <div className="grid md:grid-cols-2 md:gap-6">
+                                <div className="relative z-0 w-full mb-6 group">
+                                  <input
+                    {...register("address")}
+                    type="text"
+                    id="floating_first_name"
+                    className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    required
+                                  />
+                                  <label
+                    htmlFor="floating_first_name"
+                    className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                  >
+                    Address 
+                                  </label>
+                                </div>
+                                <div className="relative z-0 w-full mb-6 group">
+                                  <input
+                    {...register("state")}
+                    type="text"
+                    id="floating_first_name"
+                    className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    required
+                                  />
+                                  <label
+                    htmlFor="floating_first_name"
+                    className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                  >
+                    State 
+                                  </label>
+                                </div>
+                                <div className="relative z-0 w-full mb-6 group">
+                                  <input
+                    {...register("city")}
+                    type="text"
+                    id="floating_last_name"
+                    className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    required
+                                  />
+                                  <label
+                    htmlFor="floating_last_name"
+                    className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                  >
+                    City
+                                  </label>
+                                </div>
+                                <div className="relative z-0 w-full group mb-6">
+                                  <input
+                    {...register("postalCode")}
+                    type="number"
+                    id="floating_first_name"
+                    className="block py-2.5 px-0 w-full text-sm font-medium text-gray-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    required
+                                  />
+                                  <label
+                    htmlFor="floating_first_name"
+                    className="peer-focus:font-medium absolute text-sm font-medium text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                  >
+                    Postal Code
+                                  </label>
+                                </div>
+                              </div>
+                              <input
+                                type="submit"
+                                className="bg-sky-200 hover:bg-sky-300 active:border active:border-sky-400 transition-all rounded-md px-5 py-2.5 text-sm font-medium shadow cursor-pointer mt-5"
+                                value="Add User"
+                              />
+                            </form>
+                          </div>
+                        </div>
+                        <div className="md:basis-3/5 h-full">
+                            <div className="grid grid-cols-5 gap-5">
+                                {users?.users?.slice(0,20).map(each=> (
+                                    <img className="w-24 place-self-center" src={each.image} key={each.id}/>
+                                ))}
+                            </div>
+                        </div>
+                </div>
+                {/* //////////////////////// */}
+                <div className="modal-action">
+                  <form method="dialog">
+                    <button className="btn border-none hover:bg-red-200 hover:outline-none active:outline-none text-sm font-medium">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsersPage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,21 @@
 import {
   createBrowserRouter
 } from "react-router-dom";
+import ErrorPage from "../Pages/ErrorPage";
+import SingleUser from "../Pages/SingleUser";
+import UsersPage from "../Pages/UsersPage";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <UsersPage />,
+    },
+    {
+      path: "/:id",
+      element: <SingleUser />,
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
     },
   ]);
